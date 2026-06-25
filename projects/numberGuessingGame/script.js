e152c58bdcf94d8cd1e3cae7fc150e6f3dabedf0
Jugraj Singh <jugrajsinghbajwa857@gmail.com>
2026-06-25 12:38:51 +0530

    trying to build number guessing game

--- projects/numberGuessingGame/script.js
+++ projects/numberGuessingGame/script.js
@@ -1,18 +1,20 @@
-const submit=document.querySelector(".Submit");
-let UserInput=document.querySelector(".user_Value");
-let message_Text=document.querySelector(".message_Text");
-const computerNumber=[1,2,3,4,5,6,7,8,9,10];
+const Submit=document.querySelector(".Submit");
+const user_Value=document.querySelector(".user_Value");
+const message_Text=document.querySelector(".message_Text");
+const computer=[1,2,3,4,5,6,7,8,9,10];
+Submit.addEventListener("click",(event)=>{
+    const target=event.target;
+    console.log("it works");
+    let userValue=Number(user_Value.value);
+    let computerValue = computer[Math.floor(Math.random() * computer.length)];
 
-submit.addEventListener("click",()=>{
-    message_Text.innerHTML="";
-    let computer_Guess=Math.floor(Math.random()*computerNumber.length);
-    if(computer_Guess>UserInput){
-        message_Text.innerHTML="The number is low"
+    if(computerValue>userValue){
+        message_Text.innerHTML=`ur value is too low ${userValue}`;
     }
-    if(computer_Guess<UserInput){
-        message_Text.innerHTML="The number is too high"
+    if(computerValue<userValue){
+        message_Text.innerHTML=`ur value is too high ${userValue}`;
     }
-    if(computer_Guess===UserInput){
-        message_Text.innerHTML="Correct"
+    if(computerValue===userValue){
+        message_Text.innerHTML=`ur value is correct       ${userValue}`;
     }
 });
