3cb3065a99a5388293037e1601ca03bd8527f8db
Jugraj Singh <jugrajsinghbajwa857@gmail.com>
2026-06-27 09:21:06 +0530

    build this game fully funcational

--- projects/numberGuessingGame/version2/script.js
+++ projects/numberGuessingGame/version2/script.js
@@ -1,63 +1,51 @@
-// const thisgame=document.querySelector(".thisgame");
-// const userGuess=document.querySelector(".userGuess");
-// const message=document.querySelector(".message");
-// const StartBtn=document.querySelector(".startBtn");
-// const computer=[1,2,3,4,5,6,7,8,9,10];
-// let computerGuessValue=0;
-// let userGuessValue=0;
-// let guessScore=0;
-// let userScore=1;
-
-// const appState={
-
-// };
-// function gameStart(event){
-//     const target=event.target;
-//     if(target.classList.contains("StartBtn")){
-//         if(userGuess.value<1 || userGuess.value>10 || userGuess.value==""){
-//             message.innerHTML="Enter number in btw 1 to 10 ";
-//             return;
-//         }
-//         guesscompare();
-//     }
-// }
-// function guesscompare(){
-//     if(userScore===1){
-//     const computerGuess=computer[Math.floor(Math.random()*computer.length)];
-//     const userValue=Number(userGuess.value);
-//     computerGuessValue=computerGuess;
-//     userGuessValue=userGuess;
-//     compare(computerGuess,userValue);
-//     return;
-//     }
-
-// }
-// function compare(computerGuess,userValue){
-//  if(computerGuess>userValue){
-//         message.innerHTML="wrong guess ur value is too low try again";
-//         StartBtn.innerHTML="Try again";
-//         guessScore++;
-//     }
-//     if(computerGuess<userValue){
-//         message.innerHTML="wrong guess ur value is too high try again";
-//         guessScore++
-//     }
-//     if(computerGuess===userValue){
-        
-//     }
-// }
-
-
-// thisgame.addEventListener("click",gameStart);
-const appState={
-    computerGuess:null,
-    userScore:0
-}
+const thisgame=document.querySelector(".thisgame");
+const StartBtn=document.querySelector(".StartBtn");
+const userGuess=document.querySelector(".userGuess");
+const message=document.querySelector(".message");
+let guess=document.querySelector(".guess");
+let guessCounterValue=0;
 const computer=[1,2,3,4,5,6,7,8,9,10];
-function ComputerGuess(){
-    const computerGuessValue=computer[Math.floor(Math.random()*computer.length)]; 
-    appState.computerGuess=computerGuessValue;
+let computerGuessData=0;
+
+function computerGuess(){
+    const computerGuessValue=computer[Math.floor(Math.random()*computer.length)];
+    computerGuessData=computerGuessValue;
+    console.log(`computer guess ${computerGuessData}`);
 }
-function Game(){
-   
+
+thisgame.addEventListener("click",(event)=>{
+    const target=event.target;
+    if(target.classList.contains("StartBtn")){
+        if(userGuess.value>10 || userGuess.value<1 || userGuess.value===""){
+         message.innerHTML="Enter a valid number btw 1 to 10";   
+         return;
+        }
+        const userGuessValue=Number(userGuess.value);
+        compareData(userGuessValue);
+    }
+});
+function compareData(userGuessValue){
+    guessCounterValue++;
+    StartBtn.innerHTML="Guess Again"
+    let userValue=userGuessValue;
+    let computerValue=computerGuessData;
+    if(computerValue>userValue){
+        message.innerHTML="ur number is too low"
+    }
+    if(computerValue<userValue){
+        message.innerHTML="ur number is too high"
+    }
+    if(computerValue===userValue){
+        message.innerHTML="ur number is correct "
+        console.log(`ur number is correct`)
+        StartBtn.innerHTML="Play Again"
+        guess.innerHTML=0;
+        userGuess.value="";
+        guessCounterValue=0;
+        computerGuess();
+    }
+    userGuess.value="";
+    guess.innerHTML=guessCounterValue;
+
 }
+computerGuess();
