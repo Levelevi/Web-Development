24c456f0baf14c3ba3c4da96846f0082d80717dc
Jugraj Singh <jugrajsinghbajwa857@gmail.com>
2026-07-04 09:36:31 +0530

    just trying to improve it

--- projects/cardgame/script.js
+++ projects/cardgame/script.js
@@ -1,31 +1,20 @@
 const container=document.querySelector(".container");
-let cardflipedvalue=0;
-let firstcardValue=0;
-let secondcardValue=0;
-function gameStart(event){
-    const target=event.target;
-    if(target.classList.contains("box")){
-        if(cardflipedvalue>=2){
-            return;
-        }
-        cardflipedvalue++;
-        const value=target.dataset.cardValue;
-        target.innerHTML=value;
-        firstcardValue=(cardflipedvalue===1)?value:firstcardValue;
-        secondcardValue=(cardflipedvalue===2)?value:secondcardValue;
-        console.log(value);
-        console.log(`first card${firstcardValue} second card ${secondcardValue}`);
-        if(cardflipedvalue>=2){
-          comparedata();
-        }
-        }
-}
- function comparedata(){
-            if(firstcardValue===secondcardValue){
-                console.log("matched");
-            }else{
-                console.log("try again");
-            }
-        }
+let clickedcard=0;
+let firstCard=0;
+let secondCard=0;
 
-container.addEventListener("click",gameStart);
+function startGame(e){
+    const target=e.target;
+    clickedcard++;
+    firstCard=(clickedcard===1)?target:firstCard;
+    secondCard=(clickedcard===2)?target:secondCard;
+    console.log(clickedcard);
+    
+}
+
+
+
+
+
+
+container.addEventListener("click",startGame);
