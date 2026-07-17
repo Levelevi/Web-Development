80a4698432393f8ef39aae283a491eb3c1869b6e
Jugraj Singh <jugrajsinghbajwa857@gmail.com>
2026-07-17 15:32:23 +0530

    tryna learn

---
+++ projects/cartmachine/script.js
@@ -0,0 +1,38 @@
+const container=document.querySelector(".container");
+const cartBox=document.querySelector(".cart-box");
+const cart={
+    userCart:[]
+}
+function cartApp(event){
+    const target=event.target;
+    if(target.classList.contains("btn")){
+        const Element=target.closest(".product");
+        getProduct(Element);
+    }
+}
+function getProduct(productCard){
+    cart.userCart.push({
+        productName:productCard.dataset.name,
+        productPrice:productCard.dataset.price,
+        productQuantity:1
+    });
+    console.log(cart);
+    ui();
+};
+
+function ui(){
+    cartBox.innerHTML=`
+        <div>
+            <h3>Product Name${cart.userCart.productName}</h3>
+            <p>Price${cart.userCart.productPrice}</p>
+            <p>Quantity: <span>${cart.userCart.productQuantity}</span></p>
+            <button>+</button>
+            <button>-</button>
+            <button>Delete</button>
+        </div>
+    `;
+}
+
+
+
+container.addEventListener("click",cartApp);
